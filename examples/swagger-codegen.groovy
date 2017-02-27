9c9002c581cf1ac8c5c526c8b28ee54985ea57a8
eaglesakura <eagle.sakura@gmail.com>
2017-02-27 18:38:44 +0900

    add build script

---
+++ examples/swagger-codegen.groovy
@@ -0,0 +1,7 @@
+@GrabResolver(name = "eaglesakura", root = "http://eaglesakura.github.io/maven/")
+@Grab("com.eaglesakura:swagger-codegen-lw:1.0.+")
+import java.lang.Object;
+
+static main(String[] args) {
+    com.eaglesakura.swagger.generator.Generator.main(args)
+}
