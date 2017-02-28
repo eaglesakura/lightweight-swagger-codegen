ac8a3b2ce3697e46ddd8c57b4d9ffda07c20e5d2
eaglesakura <eagle.sakura@gmail.com>
2017-02-28 11:22:59 +0900

    使用例を追記 [ci skip]

--- examples/swagger-codegen.groovy
+++ examples/swagger-codegen.groovy
@@ -1,5 +1,8 @@
 #! groovy
 
+// example.
+// ./swagger-codegen.groovy generate -l com.eaglesakura.swagger.generator.AndroidCustomClientCodegen -o ./output -i swagger.json
+
 @GrabResolver(name = "eaglesakura", root = "http://eaglesakura.github.io/maven/")
 @Grab("com.eaglesakura:swagger-codegen-lw:1.0.12")
 import java.lang.Object;
