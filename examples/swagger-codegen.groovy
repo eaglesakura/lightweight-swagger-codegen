0fb7e5ec7890accd06258367ae065d653f589e31
eaglesakura <eagle.sakura@gmail.com>
2017-03-01 18:28:32 +0900

    GAE/Go, yaml2jsonのexampleを追加

--- examples/swagger-codegen.groovy
+++ examples/swagger-codegen.groovy
@@ -1,7 +1,15 @@
 #! groovy
 
 // example.
-// ./swagger-codegen.groovy generate -l com.eaglesakura.swagger.generator.AndroidCustomClientCodegen -o ./output -i swagger.json
+
+// swagger.json
+//      $ ./swagger-codegen.groovy generate -l io.swagger.codegen.languages.SwaggerGenerator -o ./output -i swagger.yaml
+
+// Android client.
+//      $ ./swagger-codegen.groovy generate -l com.eaglesakura.swagger.generator.AndroidCustomClientCodegen -o ./output -i swagger.json
+
+// GAE/Go Server.
+//      $ ./swagger-codegen.groovy generate -l com.eaglesakura.swagger.generator.GaeGoCodegen -o ./output -i swagger.json
 
 @GrabResolver(name = "eaglesakura", root = "http://eaglesakura.github.io/maven/")
 @Grab("com.eaglesakura:swagger-codegen-lw:1.0.12")
