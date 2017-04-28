494f9cdd4e461969385bdf580627cc44194b0915
eaglesakura <eagle.sakura@gmail.com>
2017-04-28 12:01:04 +0900

    自動でOperationIdを割り当てるように修正

--- src/main/java/io/swagger/codegen/languages/SwaggerGenerator.java
+++ src/main/java/io/swagger/codegen/languages/SwaggerGenerator.java
@@ -1,14 +1,20 @@
 package io.swagger.codegen.languages;
 
+import com.eaglesakura.util.StringUtil;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Map;
 
 import io.swagger.codegen.CodegenConfig;
 import io.swagger.codegen.CodegenType;
 import io.swagger.codegen.DefaultCodegen;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 import io.swagger.util.Json;
 
@@ -41,6 +47,21 @@ public class SwaggerGenerator extends DefaultCodegen implements CodegenConfig {
 
     @Override
     public void processSwagger(Swagger swagger) {
+
+        // Fixed. Auto operationId
+        for (Map.Entry<String, Path> pathEntry : swagger.getPaths().entrySet()) {
+            String key = pathEntry.getKey();
+            Path path = pathEntry.getValue();
+            for (Map.Entry<HttpMethod, Operation> opEntry : path.getOperationMap().entrySet()) {
+                HttpMethod method = opEntry.getKey();
+                Operation op = opEntry.getValue();
+                if (StringUtil.isEmpty(op.getOperationId())) {
+                    // operationIdを埋める
+                    op.setOperationId(getOrGenerateOperationId(op, key, method.name()));
+                }
+            }
+        }
+
         String swaggerString = Json.pretty(swagger);
 
         try {
@@ -53,14 +74,14 @@ public class SwaggerGenerator extends DefaultCodegen implements CodegenConfig {
     }
 
     @Override
-    public String escapeQuotationMark(String input) { 
+    public String escapeQuotationMark(String input) {
         // just return the original string
         return input;
-    } 
+    }
 
     @Override
-    public String escapeUnsafeCharacters(String input) { 
+    public String escapeUnsafeCharacters(String input) {
         // just return the original string
         return input;
-    }  
+    }
 }
