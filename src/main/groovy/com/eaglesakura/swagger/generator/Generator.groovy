e20f3238554e7d8f53d139de221370869689caf0
eaglesakura <eagle.sakura@gmail.com>
2019-11-16 23:00:14 +0900

    chg: executable jar supported to docker.

--- src/main/groovy/com/eaglesakura/swagger/generator/Generator.groovy
+++ src/main/groovy/com/eaglesakura/swagger/generator/Generator.groovy
@@ -5,7 +5,7 @@ import io.swagger.codegen.SwaggerCodegen
 class Generator {
 
     static void main(String[] args) {
-        println("Workspace :: ${new File(".").absolutePath}")
+//        println("Workspace :: ${new File(".").absolutePath}")
 //        IOUtil.cleanDirectory(new File("output"))
         SwaggerCodegen.main(args)
     }
